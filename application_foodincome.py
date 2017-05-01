95226fad3ffbb47a440e396314e13187bdbbe798
badGarnet <theyaoyou@gmail.com>
2017-04-30 20:07:41 -0500

    initializing project

---
+++ application_foodincome.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+from bokeh.embed import components
+from bokeh.plotting import figure, output_file, show
+import pandas as pd
+import numpy as np
+
+app_foodincome = Flask(__name__)
+
+app_foodincome.vars={}
+
+@app_foodincome.route('/index', methods = ['GET'])
+def main():
+    traveltime = np.loadtxt('data/traveltime.txt')
+    foodincome = pd.read_csv('data/foodincome.csv')
+    
+    wt = np.exp(-(traveltime+traveltime.T)/2000)
+    timeincome = np.dot((foodincome.fillna(foodincome.sumincome.mean())).sumincome, wt)
+    p = figure(title='number of restaurants vs. income',
+               x_axis_label='total income in a zip code area (USD)',
+               y_axis_label='number of restaurants')
+    p.scatter(timeincome, foodincome.foodsize)
+    
+    script, div = components(p)
+    return render_template('graph.html', script=script, div=div)
+
+    
+if __name__ == '__main__':
+    app_foodincome.run(debug = True)
